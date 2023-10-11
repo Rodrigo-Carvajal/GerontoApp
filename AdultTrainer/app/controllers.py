15506a41eff7175c1651119c56a95e63c0246e86
Rodrigo-Carvajal <rodrigo.carvajal.ramos@gmail.com>
2023-10-11 19:38:32 -0300

    Se agrega parte de la creación de pacientes

--- AdultTrainer/app/controllers.py
+++ AdultTrainer/app/controllers.py
@@ -2,17 +2,18 @@ from app import app, supabase, login_manager, csrf
 from flask import render_template, Response, redirect, url_for, request, flash, Blueprint, session, jsonify
 from flask_login import login_user, login_required, logout_user, current_user
 import cv2 as cv
+from app.functions import squat, pushup
 
-from app.models import squat, pushup
-from app.models import Usuario, load_user
+from app.models import Usuario, Paciente, Ejercicio, Limitacion, Registro, Sesion
 
 # Instanciación del blueprint
 adultTrain = Blueprint('app', __name__)
 
 """
-@app.route("/", methods=['GET', 'POST'])
-def ():
-    return
+@app.route("/ruta", methods=['GET', 'POST'])
+@login_required
+def ruta():
+    return render_template("views/.html") redirect(url_for(''))
 """
 
 @app.route("/")
@@ -24,12 +25,11 @@ def login():
     if request.method == 'POST':
         nombreUsuario = request.form['nombreUsuario']
         password = request.form['password']
-
-        data, count = supabase.table('Usuarios').select('*').eq('username', nombreUsuario).execute()        
+        data, count = supabase.table('Usuarios').select('*').eq('username', nombreUsuario).execute()
         if data:
-            usuarios = data[1]            
+            usuarios = data[1] 
             if usuarios:
-                usuarioData = usuarios[0]                          
+                usuarioData = usuarios[0]
                 if usuarioData["password"] == password:
                     user = Usuario(usuarioData["id"], usuarioData["username"], usuarioData["password"], usuarioData["rol"], usuarioData["nombre_completo"])
                     login_user(user)
@@ -42,7 +42,7 @@ def login():
             flash ("Usuario no encontrado", 'danger')
             return redirect(url_for('login'))
         flash ("Usuario no encontrado", 'danger')
-        return redirect(url_for('login'))    
+        return redirect(url_for('login'))
     return render_template('views/login.html')
 
 @app.route("/logout", methods=['GET', 'POST'])
@@ -68,11 +68,26 @@ def listar_pacientes():
     return render_template('views/kine/listarPacientes.html', pacientes=pacientes)
 
 @app.route("/info_paciente/<int:id_paciente>", methods=['GET', 'POST'])
+@login_required
 def info_paciente(id_paciente):
     data, count = supabase.table('Pacientes').select('*').eq('id_paciente', id_paciente).execute()
     pacientes = data[1]
     pacienteData = pacientes[0]
     return render_template("views/kine/info_paciente.html", paciente=pacienteData)
+
+@app.route("/crear_paciente/<int:id>", methods=['GET', 'POST'])
+@login_required
+def crear_paciente():
+    if request.method == 'POST':
+        pass
+    return render_template("views/kine/crear_paciente.html")
+
+@app.route("/crud_sesiones/<int:id_paciente>", methods=['GET', 'POST'])
+@login_required
+def crud_sesiones(id_paciente):
+    data, count = supabase.table('Sesiones').select('*').eq('fk_id_paciente', id_paciente).execute()
+    sesiones = data[1]
+    return render_template('views/kine/sesiones.html', sesiones=sesiones)
 
 @app.route("/video_template", methods=['GET','POST'])
 @login_required
